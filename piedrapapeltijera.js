0f739d000322c2a9ea4d01f434395d4d81ed988e
Roberto Camargo <rcgarcia@outlook.com>
2015-11-05 21:29:27 -0600

    Empiezo el juego de piedra, papel o tijera

---
+++ piedrapapeltijera.js
@@ -0,0 +1,17 @@
+var usuario = prompt("piedra, papel o tijera?");
+
+var compu = "papel";
+
+if(usuario === compu){
+	console.log("es empate =/");
+}else
+	if(usuario === "tijera"){
+	console.log("Ganas! =D");
+}else
+	if(usuario === "piedra"){
+	console.log("pierdes!");
+}
+Math.ceil(5,5)
+Math.floor(5,5)
+Math.round(5,5)
+Math.random()
