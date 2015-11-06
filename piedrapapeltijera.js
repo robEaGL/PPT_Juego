ca51bacd3c1038476e251b85682dbf62db91bff6
Roberto Camargo <rcgarcia@outlook.com>
2015-11-05 21:43:10 -0600

    Añadiendo archivo README.md

--- piedrapapeltijera.js
+++ piedrapapeltijera.js
@@ -1,4 +1,28 @@
+// En esta parte es para la desición del usuario.
 var usuario = prompt("piedra, papel o tijera?");
+
+// En esta parte vamos a calcular la desición del equipo con un valor aleatorio.
+
+var compu = function() {
+	var numero = Math.floor((Math.random()*3)+1);
+	var respuesta;
+	if (numero == 1) {
+		respuesta = "piedra";
+	}else if (numero == 2) {
+		respuesta = "papel";
+	}else {
+		respuesta = "tijera";
+	}
+}
+
+
+
+
+
+
+
+
+
 
 var compu = "papel";
 
@@ -15,3 +39,4 @@ Math.ceil(5,5)
 Math.floor(5,5)
 Math.round(5,5)
 Math.random()
+
